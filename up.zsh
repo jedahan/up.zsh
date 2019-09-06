ad30d0eecfd2e6f6354f2633d2c32aad2365c9b7
Jonathan Dahan <hi@jonathan.is>
2019-09-06 10:43:44 -0400

    Add snap, flatpak, and fwupdmgr

--- up.zsh
+++ up.zsh
@@ -1,20 +1,20 @@
 # up - upgrade everything
 
 function up {
-  local -A u
-  function fun { cmd=${1##*::}; (( $+aliases[$cmd] || $+functions[$cmd] || $+commands[$cmd] )) && { $@; u[$cmd]=$? } }
-
-  sudo -v
-
   up::config() { config pull }
   up::tldr() { tldr --update }
   up::zr() { zr update | rg 'Updating [a-f0-9]{6}\.\.[a-f0-9]{6}' -B1 }
-  up::apt() { sudo apt update && sudo apt full-upgrade -y --autoremove && sed -n '/graded:$/,/graded,/{/graded:$/b;/graded,/b;p}' }
+  up::apt() { sudo apt update -qq && sudo apt full-upgrade -y --autoremove && sed -n '/graded:$/,/graded,/{/graded:$/b;/graded,/b;p}' }
   up::nvim() { nvim +PlugUpdate! +PlugClean! +qall | rg 'Updated!\s+(.+/.+)' -r '$1' -N | paste -s - | head -c -1 }
   up::rustup() { rustup update | rg 'updated.*rustc' -N | cut -d' ' -f7 | paste -s - | head -c -1 }
   up::cargo() { cargo install-update --all | rg '(.*)Yes$' -r '$1' | paste -s - | head -c -1 }
+  up::flatpak() { flatpak update --user -y; flatpak update --system -y }
+  up::snap() { sudo snap refresh }
+  up::fwupdmgr() { fwupdmgr refresh && fwupdmgr get-updates }
 
-  for update in up::{config,tldr,zr,apt,nvim,rustup,cargo}; do fun $update; done
-
+  local -A u
+  sudo -v
+  function fun { cmd=${1##*::}; (( $+aliases[$cmd] || $+functions[$cmd] || $+commands[$cmd] )) && { $@; u[$cmd]=$? } }
+  functions | rg 'up::(\w+).*' -r '$1' | xargs -I _ fun _
   print ${(kv)u}
 }
